558d89f0b0fd4546bfd34767409fbc3680072ef9
James Garfield <jordan@grupojvm.com>
2023-10-20 16:33:58 -0500

    An example

---
+++ example/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.h"
+
+
+int main() 
+{
+    std::cout << "Hello, World from PyMakeCli!!!!" << std::endl;
+    PrintFromInclude();
+
+    return 0;
+}
---
+++ example/src/utils.cpp
@@ -0,0 +1,8 @@
+#include "utils.h"
+
+#include <iostream>
+
+void PrintFromInclude()
+{
+    std::cout << "Hello using includes" << std::endl;
+}
