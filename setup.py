478e8ad3eda740fb48c767567efdf289c487e24d
James Garfield <jordan@grupojvm.com>
2023-10-22 14:38:08 -0500

    Add click for better CLI app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymake_cli',
-    version='0.1.5',
+    version='0.1.6',
     author='Jordan Castro',
     author_email='jordan@grupojvm.com',
     packages=find_packages(),
@@ -12,11 +12,12 @@ setup(
     ],
     install_requires=[
         'pyyaml',
+        'click'
     ],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
-            'pymake-cli=pymake_cli.pymake_cli:main'
+            'pymake-cli=pymake_cli.pymake_cli:cli'
         ]
     }
 )
